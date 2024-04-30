650e2ecb2913378ed8ceab43131e37b937bac1bd
Renu1301Singh <152848609+Renu1301Singh@users.noreply.github.com>
2024-04-30 16:17:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+  {
+        "@reportportal/agent-js-postman": {
+            "endpoint": "http://nashtech-test-reportportal.eastasia.cloudapp.azure.com:8080",
+            "token": "00000000-0000-0000-0000-000000000000",
+            "launch": "RENU",
+            "project": "automation",
+            "description": "postman test"
+        }
+    }
